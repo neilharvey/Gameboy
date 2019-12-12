a25a624fd3156d4f82a0590500717558d2f12811
Neil Harvey <neil.harvey@hotmail.co.uk>
2019-12-12 13:40:08 +0000

    Add definitions for all instructions

--- Gameboy/src/Cpu.cpp
+++ Gameboy/src/Cpu.cpp
@@ -10,7 +10,7 @@ Cpu::Cpu(Mmu& m) : mmu(m) {
 }
 
 void Cpu::tick() {
-	auto opcode = read_byte();
+	auto opcode = next_byte();
 	execute_opcode(opcode);
 }
 
@@ -20,64 +20,19 @@ void Cpu::execute_opcode(const byte& opcode) {
 	{
 		case 0x00: nop();						break;
 
-		case 0x06: ld_r_n(&b, read_byte());		break;
-		case 0x0E: ld_r_n(&c, read_byte());		break;
-		case 0x16: ld_r_n(&d, read_byte());		break;
-		case 0x1E: ld_r_n(&e, read_byte());		break;
-		case 0x26: ld_r_n(&h, read_byte());		break;
-		case 0x2E: ld_r_n(&l, read_byte());		break;
-
-		case 0x7F: ld_a_n(a);					break;
-		case 0x78: ld_a_n(b);					break;
-		case 0x79: ld_a_n(c);					break;
-		case 0x7A: ld_a_n(d);					break;
-		case 0x7B: ld_a_n(e);					break;
-		case 0x7C: ld_a_n(h);					break;
-		case 0x7D: ld_a_n(l);					break;
-		case 0x0A: ld_a_n(mmu.read(bc));		break;
-		case 0x1A: ld_a_n(mmu.read(de));		break;
-		case 0x7E: ld_a_n(mmu.read(hl));		break;
-		//case 0xFA: ld_a_n(read_byte()); break;
-		case 0x3E: ld_a_n(read_byte());			break;
-
-		case 0x01: ld_n_nn(&bc, read_word());	break;
-		case 0x11: ld_n_nn(&de, read_word());	break;
-		case 0x21: ld_n_nn(&hl, read_word());	break;
-		case 0x31: ld_n_nn(&sp, read_word());	break;
-			
-		case 0x01: ld_n_a(bc, a);				break;
-
 		default:
 			throw "Opcode not implemented " + opcode;
 	}
 	// clang-format on
 }
 
-void Cpu::nop() {
-}
-
-void Cpu::ld_r_n(&byte r, byte n) {
-	r = n;
-}
-
-void Cpu::ld_n_nn(word& n, word nn) {
-	n = nn;
-}
-
-void Cpu::ld_a_n(byte n) { 
-	a = n; 
-}
-
-void Cpu::ld_n_a(byte& n) {
-}
-
-byte Cpu::read_byte() {
+byte Cpu::next_byte() {
 	return mmu.read(pc++);
 }
 
-word Cpu::read_word() {
-	byte lo = read_byte();
-	byte hi = read_byte();
+word Cpu::next_word() {
+	byte lo = next_byte();
+	byte hi = next_byte();
 	word word = ((hi << 8) + lo);
 	return word;
 }
--- Gameboy/src/Cpu.h
+++ Gameboy/src/Cpu.h
@@ -2,6 +2,13 @@
 #include "Mmu.h"
 #include "Types.h"
 
+enum class Condition {
+	NZ,
+	Z,
+	NC,
+	C
+};
+
 class Cpu
 {
 public:
@@ -50,15 +57,96 @@ private:
 
 	Mmu& mmu;
 
-	// instructions
-	void nop(void);
-	void ld_r_n(&byte r, byte n);
-	void ld_n_nn(word& reg, word nn);
-
+	// 8-bit loads 
+	void ld(byte& r, byte n);
 	void ld_a_n(byte n);
+	void ld_n_a(byte n);
+	void ld_a_c(byte c);
+	void ld_c_a(byte c);
+	void ldd_a_hl();
+	void ldd_hl_a();
+	void ldi_a_hl();
+	void ldi_hl_a();
+	void ldh_n_a(byte n);
+	void ldh_a_b(byte n);
+
+	// 16-bit loads
+	void ld(word& r, word nn);
+	void ld_sp_hl();
+	void ldhl_sp(byte n);
+	void ld_sp(word nn);
+	void push(word nn);
+	void pop(word nn);
+
+	// 8-bit ALU
+	void add(byte n);
+	void adc(byte n);
+	void sub(byte n);
+	void sbc(byte n);
+	void and(byte n);
+	void or(byte n);
+	void xor(byte n);
+	void cp(byte n);
+	void inc(byte& r);
+	void dec(byte& r);
+
+	// 16-bit arithmetic
+	void add_hl(word n);
+	void add_sp(byte n);
+	void inc(word& r);
+	void dec(word& r);
+
+	// miscellaneous
+	void swap(byte n);
+	void daa();
+	void cpl();
+	void ccf();
+	void scf();
+	void nop();
+	void halt();
+	void stop();
+	void di();
+	void ei();
+
+	// rotates & Shifts
+	void rlca();
+	void rla();
+	void rrca();
+	void rra();
+	void rlc(byte n);
+	void rl(byte b);
+	void rrc(byte n);
+	void rr(byte n);
+	void sla(byte n);
+	void sra(byte n);
+	void srl(byte n);
+
+	// bitwise 
+	void bit(int b, byte r);
+	void set(int b, byte& r);
+	void res(int b, byte& r);
+
+	// jumps
+	void jp(word nn);
+	void jp(Condition cc, word nn);
+	void jr(byte n);
+	void jr(Condition cc, byte n);
+
+	// calls
+	void call(word nn);
+	void call(Condition cc, word nn);
+
+	// Restarts
+	void rst(byte n);
+	void ret();
+	void ret(Condition cc);
+	void reti();
 
 	// memory
-	byte read_byte();
-	word read_word();
+	byte next_byte();
+	word next_word();
+
+	// utility
+	void is_true(Condition cc);
 };
 
