d734fdf9b6f55f6c59ced645262042cecedde053
Neil Harvey <neil.harvey@hotmail.co.uk>
2020-03-13 23:38:14 +0000

    Add display

--- src/gameboy/display.cpp
+++ src/gameboy/display.cpp
@@ -1,9 +1,14 @@
 #include "display.h"
 
-void Display::set_pixel(byte x, byte y, byte color) {
-
+void Display::Display() {
+    pixels(WIDTH * HEIGHT, 0);
 }
 
-void Display::refresh() {
+void Display::set_pixel(int x, int y, int color) {
+     pixels[(y * WIDTH) + x] = color;
+}
 
+int Display::get_pixel(int x, int y) const
+{
+    return pixels[(y * WIDTH) + x];
 }
--- src/gameboy/display.h
+++ src/gameboy/display.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <vector>
+
 #include "types.h"
 
 class Display {
 public:
-	void set_pixel(byte x, byte y, byte color);
-	void refresh();
+
+	const int WIDTH = 160;
+	const int HEIGHT = 144;
+
+	void set_pixel(int x, int y, int color);
+	int get_pixel(int x, int y) const;
+private:
+	std::vector<int> pixels;
 };
