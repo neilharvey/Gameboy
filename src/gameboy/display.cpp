b500783279589a0c2bd302a47127f064d601faf3
Neil Harvey <neil.harvey@hotmail.co.uk>
2020-03-14 16:25:55 +0000

    Fix some warnings

--- src/gameboy/display.cpp
+++ src/gameboy/display.cpp
@@ -1,14 +1,20 @@
 #include "display.h"
 
-void Display::Display() {
-    pixels(WIDTH * HEIGHT, 0);
+Display::Display() :
+    pixels(WIDTH * HEIGHT, 0)
+{
 }
 
 void Display::set_pixel(int x, int y, int color) {
-     pixels[(y * WIDTH) + x] = color;
+     pixels[pixel_index(x, y)] = color;
 }
 
 int Display::get_pixel(int x, int y) const
 {
-    return pixels[(y * WIDTH) + x];
+    return pixels[pixel_index(x,y)];
+}
+
+int Display::pixel_index(int x, int y) const
+{
+    return x + (y * WIDTH);
 }
--- src/gameboy/display.h
+++ src/gameboy/display.h
@@ -10,8 +10,10 @@ public:
 	const int WIDTH = 160;
 	const int HEIGHT = 144;
 
+	Display();
 	void set_pixel(int x, int y, int color);
 	int get_pixel(int x, int y) const;
 private:
 	std::vector<int> pixels;
+	int pixel_index(int x, int y) const;
 };
